bca6e819a2b48ba023b7c879203d757732200da5
skewondr <skewondr@naver.com>
2022-12-01 03:31:11 +0900

    table2line, save json bbox labels

--- main.py
+++ main.py
@@ -48,28 +48,27 @@ for i, queries_tables in enumerate(dataset.train_samples):
     val, _ = Table2Line(table, i, queries_tables["answer"])
     cnt.append(val)
     if val == 4:
-        print(i)
         fig_name, bbfig_name, bbox_list = _
         plot_list[i]=(fig_name, bbfig_name, bbox_list)
         # break 
-    if i>300: break
+    # if i>300: break
 save_bbox(plot_list, "train")
 
-# print("total possible line plots:", Counter(cnt))
+print("total possible line plots:", Counter(cnt))
 
-# plot_list = dict()
-# for i, queries_tables in enumerate(dataset.dev_samples):
-#     try:
-#         table = pd.DataFrame.from_records(queries_tables["table"][1:], columns=queries_tables["table"][0]).astype(str)
-#     except:
-#         continue
-#     val = Table2Line(table, i, queries_tables["answer"])
-#     cnt.append(val)
-#     if val == 4: 
-#         fig_name, bbfig_name, bbox_list = _
-#         plot_list[i]=(fig_name, bbfig_name, bbox_list)
-#         # break
-# save_bbox(plot_list, "dev")
+plot_list = dict()
+for i, queries_tables in enumerate(dataset.dev_samples):
+    try:
+        table = pd.DataFrame.from_records(queries_tables["table"][1:], columns=queries_tables["table"][0]).astype(str)
+    except:
+        continue
+    val = Table2Line(table, i, queries_tables["answer"])
+    cnt.append(val)
+    if val == 4: 
+        fig_name, bbfig_name, bbox_list = _
+        plot_list[i]=(fig_name, bbfig_name, bbox_list)
+        # break
+save_bbox(plot_list, "dev")
 
 
-# print("total possible line plots:", Counter(cnt))
+print("total possible line plots:", Counter(cnt))
